da473e3f82ee35380687c55ff6bd5fc3db52530b
Daylin Morgan <me@dayl.in>
2025-02-10 15:38:22 -0600

    chore: bump version

--- src/swydd/__init__.py
+++ src/swydd/__init__.py
@@ -21,7 +21,7 @@ if TYPE_CHECKING:
     from subprocess import CompletedProcess
     from typing import Any, Callable, Dict, List, Optional, Tuple
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 # TODO: make this ouput wider help when necessary
